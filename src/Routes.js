3318c898e7363903fe79f4b973bd41f03aa2e8f8
jimin <jm131313.kim@gmail.com>
2021-06-07 17:35:28 +0900

    Add: 초기 개발 환경 셋팅

---
+++ src/Routes.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Main from './Pages/Main/Main';
+import Join from './Pages/Join/Join';
+import Login from './Pages/Login/Login';
+import Category from './Pages/Category/Category';
+import Product from './Pages/Product/Product';
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/main" component={Main} />
+          <Route exact path="/join" component={Join} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/category" component={Category} />
+          <Route exact path="/profuct" component={Product} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default Routes;
